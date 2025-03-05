d3acc8d4cad8f9ae6ae35fa7191b8de68cd86f6f
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-03-05 18:38:51 +0200

    installed vue

--- vue/vue-project/src/AppDefault.vue
+++ vue/vue-project/src/AppDefault.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import IconFlagBritain from '@/assets/icons/flag-britain.svg'
+</script>
 
 <template>
-  <main>Hello world</main>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <IconFlagBritain class="icon" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
