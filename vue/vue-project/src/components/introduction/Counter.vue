82c49abfeebd5caeded4bd7dfbddee1bda017d12
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-03-05 19:23:22 +0200

    introduction

---
+++ vue/vue-project/src/components/introduction/Counter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+onMounted(() => {
+  console.log(`Iitial count is ${count.value}`);
+});
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
---
+++ vue/vue-project/src/views/intoduction/IntroductionView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Counter from '@/components/introduction/Counter.vue';
+</script>
+
+<template>
+  <div>
+    <h1>This is an introduction page</h1>
+    <Counter />
+  </div>
+</template>
