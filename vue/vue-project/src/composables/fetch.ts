948e05e6f040f6730413872b87f35cd8ba8f2be4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-07 18:25:01 +0300

    composables - accepting reactive state

--- vue/vue-project/src/composables/fetch.ts
+++ vue/vue-project/src/composables/fetch.ts
@@ -1,17 +1,40 @@
-import { ref } from 'vue'
+import { ref, toValue, watchEffect } from 'vue'
 
-export function useFetch(url: string) {
+export function useFetch(url: any) {
   const data = ref(null)
-  const loading = ref(true)
-  const error = ref(null)
+  const error = ref<string | null>(null)
+  const isLoading = ref(false)
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((json) => (data.value = json))
-    .catch((err) => (error.value = err))
-    .finally(() => {
-      loading.value = false
-    })
+  watchEffect(async () => {
+    data.value = null
+    error.value = null
+    isLoading.value = true
 
-  return { data, loading, error }
+    const urlValue = toValue(url)
+
+    try {
+      await timeout()
+
+      const res = await fetch(urlValue)
+      data.value = await res.json()
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : String(e)
+    } finally {
+      isLoading.value = false
+    }
+  })
+
+  return { data, error, isLoading }
+}
+
+function timeout() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.3) {
+        resolve(1)
+      } else {
+        reject(new Error('Random Error'))
+      }
+    }, 300)
+  })
 }
--- vue/vue-project/src/views/reusability/Composables.vue
+++ vue/vue-project/src/views/reusability/Composables.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { useFetch } from '@/composables/fetch';
 import { useMouse } from '@/composables/mouse';
+import { computed, ref } from 'vue';
 
 const { x, y } = useMouse()
-const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/todos/1')
+const todoId = ref(1);
+const url = computed(() => `https://jsonplaceholder.typicode.com/todos/${todoId.value}`)
+const { data, isLoading, error } = useFetch(url)
 </script>
 
 <template>
   <h1>Composables</h1>
   <p>Mouse position is at: {{ x }}, {{ y }}</p>
-  <p v-if="loading">Loading...</p>
-  <p v-if-else="data">{{ data }}</p>
+  <p v-if="data">{{ data }}</p>
+  <p v-else-if="isLoading">Loading...</p>
   <p v-if="error">{{ error }}</p>
+  <div>
+    <button @click="todoId++">Next todo</button>
+  </div>
 </template>
