96a858f943c4620d7af7cb29aac2566b6ca126bb
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-07 12:09:03 +0300

    slots

---
+++ vue/vue-project/src/components/slots/FancyButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="btn">
+    <slot>Fallback content</slot>
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+</style>
--- vue/vue-project/src/views/components-in-depth/SlotsView.vue
+++ vue/vue-project/src/views/components-in-depth/SlotsView.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FancyButton from '@/components/slots/FancyButton.vue';
+
+const buttonTitle = 'Click me';
+
+</script>
 
 <template>
   <h1>Slots</h1>
+  <div>Button title: {{ buttonTitle }}</div>
+  <div>
+    <FancyButton>{{ buttonTitle }}</FancyButton>
+  </div>
+  <div>
+    <FancyButton />
+  </div>
 </template>
