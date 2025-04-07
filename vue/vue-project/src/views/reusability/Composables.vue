421fa7571b4942de88ac9bec84541d0dfa5d21cc
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-07 17:41:43 +0300

    composables - mouse tracker example

--- vue/vue-project/src/views/reusability/Composables.vue
+++ vue/vue-project/src/views/reusability/Composables.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMouse } from '@/composables/mouse';
+
+const { x, y } = useMouse()
+</script>
 
 <template>
   <h1>Composables</h1>
+  <p>Mouse position is at: {{ x }}, {{ y }}</p>
 </template>
