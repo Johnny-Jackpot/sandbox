e92c65c0a63bd7347ef65f955622ea376f78dfdf
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-12 15:42:15 +0300

    reusable transitions

--- vue/vue-project/src/components/build-in-components/transition/JavascriptHooks.vue
+++ vue/vue-project/src/components/build-in-components/transition/JavascriptHooks.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import gsap from 'gsap';
-
-const show = ref(true)
 
 
 function onBeforeEnter(el: Element): void {
@@ -41,18 +38,7 @@ function onLeave(el: Element, done: () => void): void {
 </script>
 
 <template>
-  <button @click="show = !show">Toggle</button>
   <Transition @before-enter="onBeforeEnter" @enter="onEnter" @leave="onLeave">
-    <div class="gsap-box" v-if="show"></div>
+    <slot></slot>
   </Transition>
 </template>
-
-<style scoped>
-.gsap-box {
-  background: red;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  margin: 20px;
-}
-</style>
--- vue/vue-project/src/views/build-in-components/TransitionView.vue
+++ vue/vue-project/src/views/build-in-components/TransitionView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import CssBasedTransitions from '@/components/build-in-components/transition/CssBasedTransitions.vue';
 import JavascriptHooks from '@/components/build-in-components/transition/JavascriptHooks.vue';
 import NestedTransitions from '@/components/build-in-components/transition/NestedTransitions.vue';
 import TransitionBasicUsage from '@/components/build-in-components/transition/TransitionBasicUsage.vue';
 
+const show = ref(true)
 
 </script>
 
@@ -12,5 +15,18 @@ import TransitionBasicUsage from '@/components/build-in-components/transition/Tr
   <TransitionBasicUsage />
   <CssBasedTransitions />
   <NestedTransitions />
-  <JavascriptHooks />
+  <button @click="show = !show">Toggle</button>
+  <JavascriptHooks>
+    <div v-if="show" class="gsap-box"></div>
+  </JavascriptHooks>
 </template>
+
+<style scoped>
+.gsap-box {
+  background: red;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin: 20px;
+}
+</style>
