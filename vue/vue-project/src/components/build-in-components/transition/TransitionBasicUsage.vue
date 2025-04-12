778d5b68a935aed9d8353c19386790b99b98bbd1
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-12 14:27:09 +0300

    basic transition usage

---
+++ vue/vue-project/src/components/build-in-components/transition/TransitionBasicUsage.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const show = ref(true)
+</script>
+
+<template>
+  <button @click="show = !show">Toggle</button>
+  <Transition>
+    <p v-if="show">Hello world</p>
+  </Transition>
+  <Transition name="fade">
+    <p v-show="show">Hello world</p>
+  </Transition>
+  <Transition name="slide-fade">
+    <p v-show="show">Hello world</p>
+  </Transition>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active,
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to,
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+</style>
--- vue/vue-project/src/views/build-in-components/TransitionView.vue
+++ vue/vue-project/src/views/build-in-components/TransitionView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import TransitionBasicUsage from '@/components/build-in-components/transition/TransitionBasicUsage.vue';
+
 
 </script>
 
 <template>
   <h1>Transition</h1>
-
+  <TransitionBasicUsage />
 </template>
