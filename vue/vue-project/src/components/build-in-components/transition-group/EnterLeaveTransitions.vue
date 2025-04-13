00d233a9a58051da212bdf70de93195467c5fef3
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-13 16:05:37 +0300

    transition group

---
+++ vue/vue-project/src/components/build-in-components/transition-group/EnterLeaveTransitions.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const getItems = () => [1, 2, 3, 4, 5]
+
+const items = ref(getItems())
+
+let id = items.value.length + 1;
+
+const getRandomIndex = () => Math.round(Math.random() * items.value.length)
+
+function insert() {
+  items.value.splice(getRandomIndex(), 0, id++)
+}
+
+function remove() {
+  items.value.splice(getRandomIndex(), 1)
+}
+
+function reset() {
+  items.value = getItems()
+  id = items.value.length + 1
+}
+</script>
+
+<template>
+  <button @click="insert">Insert at random index</button>
+  <button @click="remove">Remove at random index</button>
+  <button @click="reset">Reset</button>
+  <TransitionGroup name="list" tag="ul">
+    <li v-for="item in items" :key="item">{{ item }}</li>
+  </TransitionGroup>
+</template>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5 ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
--- vue/vue-project/src/views/build-in-components/TransitionGroupView.vue
+++ vue/vue-project/src/views/build-in-components/TransitionGroupView.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
+import EnterLeaveTransitions from '@/components/build-in-components/transition-group/EnterLeaveTransitions.vue';
+
 
 </script>
 
 <template>
   <div style="margin-bottom: 100px;">
     <h1>Transition Group</h1>
-
+    <EnterLeaveTransitions />
   </div>
 </template>
 
-<style scoped>
-.gsap-box {
-  background: red;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  margin: 20px;
-}
-</style>
+<style scoped></style>
