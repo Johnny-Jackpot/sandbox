e2b6a456a1fb0a9495b3d3cfa69ba9644941bf3e
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-15 16:42:32 +0300

    added form for a new todo

--- vue/vue-project/src/components/scaling-up/testing/todo-list/TodoList.vue
+++ vue/vue-project/src/components/scaling-up/testing/todo-list/TodoList.vue
@@ -8,9 +8,21 @@ const todos = ref([
     completed: false,
   }
 ])
+const newTodo = ref('')
+
+function createTodo() {
+  todos.value.push({
+    id: 2,
+    text: newTodo.value,
+    completed: false
+  })
+}
 </script>
 
 <template>
+  <form data-test="form" @submit.prevent="createTodo">
+    <input type="text" data-test="new-todo" v-model="newTodo" />
+  </form>
   <ul>
     <li v-for="todo in todos" :key="todo.id" data-test="todo">
       {{ todo.text }}
