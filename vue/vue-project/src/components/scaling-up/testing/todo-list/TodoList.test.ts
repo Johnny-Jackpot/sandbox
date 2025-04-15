e124e4f0b73fc209432c87ee153250c821e7efa4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-15 16:38:03 +0300

    added 'create todo' test

--- vue/vue-project/src/components/scaling-up/testing/todo-list/TodoList.test.ts
+++ vue/vue-project/src/components/scaling-up/testing/todo-list/TodoList.test.ts
@@ -10,4 +10,15 @@ describe('TodoList tests', () => {
 
     expect(todo.text()).toBe('Learn Vue.js 3')
   })
+
+  test('creates a todo', () => {
+    const wrapper = mount(TodoList)
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1)
+
+    wrapper.get('[data-test="new-todo"]').setValue('New todo')
+    wrapper.get('[data-test="form"]').trigger('submit')
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+  })
 })
