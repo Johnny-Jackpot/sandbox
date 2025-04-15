1ae663faf3a2e9ac6f9bd63d3ce96b2ab0d15291
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-15 17:35:38 +0300

    added test for rendering admin link

--- vue/vue-project/src/components/scaling-up/testing/conditional-rendering/ConditionalRendering.test.ts
+++ vue/vue-project/src/components/scaling-up/testing/conditional-rendering/ConditionalRendering.test.ts
@@ -1,13 +1,18 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
-import ConditionalRenderingCopy from './ConditionalRendering.vue'
+import ConditionalRendering from './ConditionalRendering.vue'
 
 describe('Conditional rendering component', () => {
-  test('renders a profile link', () => {
-    const wrapper = mount(ConditionalRenderingCopy)
+  test('Renders a profile link', () => {
+    const wrapper = mount(ConditionalRendering)
 
     const profileLink = wrapper.get('#profile')
 
     expect(profileLink.text()).toEqual('My profile')
+  })
+
+  test('Does not render an admin link', () => {
+    const wrapper = mount(ConditionalRendering)
+    expect(wrapper.find('#admin').exists()).toBe(false)
   })
 })
