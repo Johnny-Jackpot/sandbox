a292b433d5ca467e534b76aa872d2071f42caf42
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-18 15:23:26 +0300

    added simple form

---
+++ vue/vue-project/src/components/scaling-up/testing/testing-forms/InteractingWithFormElements.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('')
+const emit = defineEmits<{
+  submit: [string]
+}>()
+
+const submit = () => {
+  emit('submit', email.value)
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="email">
+    <button @click="submit">Submit</button>
+  </div>
+</template>
+
+<style scoped></style>
--- vue/vue-project/src/views/scaling-up/VueTestUtilsCrashCourse.vue
+++ vue/vue-project/src/views/scaling-up/VueTestUtilsCrashCourse.vue
@@ -2,6 +2,7 @@
 import ConditionalRendering from '@/components/scaling-up/testing/conditional-rendering/ConditionalRendering.vue';
 import TodoList from '@/components/scaling-up/testing/todo-list/TodoList.vue';
 import TestingEmittedEvents from '@/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.vue';
+import InteractingWithFormElements from '@/components/scaling-up/testing/testing-forms/InteractingWithFormElements.vue';
 
 
 </script>
@@ -11,6 +12,7 @@ import TestingEmittedEvents from '@/components/scaling-up/testing/testing-emitte
   <TodoList />
   <ConditionalRendering />
   <TestingEmittedEvents />
+  <InteractingWithFormElements />
 </template>
 
 <style scoped></style>
