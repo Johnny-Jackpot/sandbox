f0fa34c7ab121f4c49e82c233176a863e5fcbce4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-18 14:33:50 +0300

    testing emitted events - added component

---
+++ vue/vue-project/src/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+const emit = defineEmits<{
+  increment: [number]
+}>()
+
+function increment() {
+  count.value++
+  emit('increment', count.value)
+}
+</script>
+
+<template>
+  <button @click="increment">Increment</button>
+</template>
+
+<style scoped></style>
--- vue/vue-project/src/views/scaling-up/VueTestUtilsCrashCourse.vue
+++ vue/vue-project/src/views/scaling-up/VueTestUtilsCrashCourse.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ConditionalRendering from '@/components/scaling-up/testing/conditional-rendering/ConditionalRendering.vue';
 import TodoList from '@/components/scaling-up/testing/todo-list/TodoList.vue';
+import TestingEmittedEvents from '@/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.vue';
 
 
 </script>
@@ -9,6 +10,7 @@ import TodoList from '@/components/scaling-up/testing/todo-list/TodoList.vue';
   <h1>Vue test utils crash course</h1>
   <TodoList />
   <ConditionalRendering />
+  <TestingEmittedEvents />
 </template>
 
 <style scoped></style>
