f09c55ffab78fb63012dbcc2721e8333b2a40946
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-18 15:04:29 +0300

    test event argumets

--- vue/vue-project/src/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.test.ts
+++ vue/vue-project/src/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.test.ts
@@ -2,6 +2,8 @@ import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
 import TestingEmittedEvents from './TestingEmittedEvents.vue'
 
+const incrementEventName = 'increment'
+
 describe('Testing emitted events', () => {
   test('emits an event when clicked', () => {
     const wrapper = mount(TestingEmittedEvents)
@@ -9,6 +11,20 @@ describe('Testing emitted events', () => {
     wrapper.find('button').trigger('click')
     wrapper.find('button').trigger('click')
 
-    expect(wrapper.emitted()).toHaveProperty('increment')
+    expect(wrapper.emitted()).toHaveProperty(incrementEventName)
+  })
+
+  test('emits an event with count argument when clicked', () => {
+    const wrapper = mount(TestingEmittedEvents)
+
+    wrapper.find('button').trigger('click')
+    wrapper.find('button').trigger('click')
+
+    const incrementEvent = wrapper.emitted(incrementEventName)
+
+    expect(incrementEvent).toBeDefined()
+    expect(incrementEvent).toHaveLength(2)
+    expect(incrementEvent![0]).toEqual([1])
+    expect(incrementEvent![1]).toEqual([2])
   })
 })
