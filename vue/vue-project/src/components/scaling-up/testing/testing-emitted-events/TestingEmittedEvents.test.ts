fa446b3d016a52909f576621280c3746690672c4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-04-18 15:11:14 +0300

    asserting complex event test added

--- vue/vue-project/src/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.test.ts
+++ vue/vue-project/src/components/scaling-up/testing/testing-emitted-events/TestingEmittedEvents.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, test } from 'vitest'
 import TestingEmittedEvents from './TestingEmittedEvents.vue'
 
 const incrementEventName = 'increment'
+const complexIncrementEventName = 'complexIncrement'
 
 describe('Testing emitted events', () => {
   test('emits an event when clicked', () => {
@@ -26,5 +27,18 @@ describe('Testing emitted events', () => {
     expect(incrementEvent).toHaveLength(2)
     expect(incrementEvent![0]).toEqual([1])
     expect(incrementEvent![1]).toEqual([2])
+  })
+
+  test('complexIncrement event has correct arguments', () => {
+    const wrapper = mount(TestingEmittedEvents)
+
+    wrapper.find('button').trigger('click')
+    wrapper.find('button').trigger('click')
+
+    const complexIncrement = wrapper.emitted(complexIncrementEventName)
+    expect(complexIncrement).toBeDefined()
+    expect(complexIncrement).toHaveLength(2)
+    expect(complexIncrement![0]).toEqual([{ count: 1, isEven: false }])
+    expect(complexIncrement![1]).toEqual([{ count: 2, isEven: true }])
   })
 })
