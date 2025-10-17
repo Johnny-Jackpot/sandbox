2a6bb585a3e5b0f116ad6cd3f111bf11f4ec2d22
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-10-17 18:28:15 +0300

    algorithms and data structures: set

--- algorithms-and-data-structures/sets/sets.ts
+++ algorithms-and-data-structures/sets/sets.ts
@@ -8,3 +8,20 @@ visitors.add("user123").add("user345");
 if (visitors.has("user123")) {
   console.log("This user has visited before");
 }
+
+let library: Set<string> = new Set(); // Tracks unique book titles
+
+library.add("War and Peace");
+library.add("1984");
+library.add("1984");
+library.add("1984");
+library.add("1984");
+library.add("Pride and Prejudice");
+
+console.log(library.has("War and Peace")); // Should log 'true'
+console.log(library);
+console.log(library.size);
+
+const bookCollection: Set<string> = new Set();
+bookCollection.add("The Great Gatsby").add("To Kill a Mockingbird");
+console.log(bookCollection.size);
