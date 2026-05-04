e0679390e53863f8927dedb4cfc5a6a6bf02207d
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2026-05-04 16:37:48 +0300

    Add logging functionality to UserManager and update tests

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -1,8 +1,15 @@
+export interface ILogger {
+  log(message: string): void;
+}
+
 export class UserManager {
+  public constructor(private logger: ILogger) {}
+
   private users: string[] = [];
 
   public addUser(user: string): this {
     this.users.push(user);
+    this.logger.log(`User ${user} added`);
     return this;
   }
 
--- tests/UserManager.test.ts
+++ tests/UserManager.test.ts
@@ -1,15 +1,31 @@
-import { UserManager } from '../src/UserManager';
+import { ILogger, UserManager } from '../src/UserManager';
+
+class FakeLogger implements ILogger {
+  public logs: string[] = [];
+  
+  log(message: string): void {
+    this.logs.push(message);
+  }
+}
 
 describe('UserManager', () => {
   let userManager: UserManager;
+  let logger: FakeLogger;
 
   beforeEach(() => {
-    userManager = new UserManager();
+    logger = new FakeLogger();
+    userManager = new UserManager(logger);
   });
 
   it('addUser adds a valid user', () => {
     const user = 'john';
     userManager.addUser(user);
     expect(userManager.getUsers()).toContain(user);
+  })
+
+  it('addUser logs when we add a user', () => {
+    const user = 'john';
+    userManager.addUser(user);
+    expect(logger.logs).toContain('User john added');
   })
 });
